fd9cc5059c0afe143f3d9e8c58aba3a0521be140
MarcelCode <marcel.siegmann@code.berlin>
2021-03-31 11:57:50 +0200

    clean code

--- handler.go
+++ handler.go
@@ -6,62 +6,49 @@ import (
 	"net/http"
 )
 
-func LightOn(c echo.Context) (err error) {
-	switch c.Param("id") {
+func getModule(id string) (module Module) {
+	switch id {
 	case "1":
-		go Modules.module1.lightOn()
+		module = Modules.module1
 	case "2":
-		go Modules.module2.lightOn()
+		module = Modules.module2
 	default:
 		fmt.Println("Module not implemented")
 	}
+	return
+}
+
+func LightOn(c echo.Context) (err error) {
+	id := c.Param("id")
+	module := getModule(id)
+	module.lightOn()
 	return c.JSON(http.StatusOK, "light on")
 }
 
 func LightOff(c echo.Context) (err error) {
-	switch c.Param("id") {
-	case "1":
-		go Modules.module1.lightOff()
-	case "2":
-		go Modules.module2.lightOff()
-	default:
-		fmt.Println("Module not implemented")
-	}
+	id := c.Param("id")
+	module := getModule(id)
+	module.lightOff()
 	return c.JSON(http.StatusOK, "light off")
 }
 
 func BreathOn(c echo.Context) (err error) {
-	switch c.Param("id") {
-	case "1":
-		go Modules.module1.breathOn()
-	case "2":
-		go Modules.module2.breathOn()
-	default:
-		fmt.Println("Module not implemented")
-	}
+	id := c.Param("id")
+	module := getModule(id)
+	module.breathOn()
 	return c.JSON(http.StatusOK, "breath on")
 }
 
 func BreathOff(c echo.Context) (err error) {
-	switch c.Param("id") {
-	case "1":
-		go Modules.module1.breathOff()
-	case "2":
-		go Modules.module2.breathOff()
-	default:
-		fmt.Println("Module not implemented")
-	}
+	id := c.Param("id")
+	module := getModule(id)
+	module.breathOff()
 	return c.JSON(http.StatusOK, "breath off")
 }
 
 func State(c echo.Context) (err error) {
-	switch c.Param("id") {
-	case "1":
-		fmt.Println(Modules.module1.State)
-	case "2":
-		fmt.Println(Modules.module2.State)
-	default:
-		fmt.Println("Module not implemented")
-	}
+	id := c.Param("id")
+	module := getModule(id)
+	fmt.Println(module.State)
 	return c.JSON(http.StatusOK, "state")
 }
